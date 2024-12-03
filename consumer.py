3e760eef405d45c0bc4b15791154bcc4e26df4c0
gmcreynolds <gabrielmc785@gmail.com>
2024-12-02 18:13:42 -0600

    batches now run continously

--- consumer.py
+++ consumer.py
@@ -1,17 +1,28 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+import time
 
 from main import CHECKPOINT_LOCATION
 
 KAFKA_TOPIC_NAME = "posStreaming"
 KAFKA_BOOTSTRAP_SERVERS = "localhost:9092"
 
+def process_batch(df, epoch_id):
+    start_time = time.time()
+    aggregated_df = df.groupBy("store_id", "item_id").agg(sum("quantity").alias("delta_quantity")).orderBy(
+        "delta_quantity", ascending=True)
+    end_time = time.time()
+    duration = end_time - start_time
+    print(f"Batch {epoch_id} GroupBy operation took {duration:.5f} seconds")
+    aggregated_df.show()
+
 if __name__ == "__main__":
     spark = (
         SparkSession.builder.appName("CIS533 POS Streaming Consumer")
         .master("local[*]")
         .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.3")
+        .config("spark.streaming.concurrentJobs", "4")
         .getOrCreate()
     )
     spark.sparkContext.setLogLevel("ERROR")
@@ -37,16 +48,9 @@ if __name__ == "__main__":
         .add("change_type_id", IntegerType())
     )
 
-    # Merge df with schema
     info_dataframe = base_df.select(
         from_json(col("value"), inventory_changed_schema).alias("data"), "timestamp"
     ).select("data.*", "timestamp")
 
-    info_dataframe.printSchema()
-
-    aggregated_df = info_dataframe.groupBy("store_id", "item_id").agg(sum("quantity").alias("delta_quantity")).orderBy(
-        "delta_quantity", ascending=True)
-
-    query = aggregated_df.writeStream.format("console").trigger(
-        processingTime='5 seconds').outputMode("complete").start()
+    query = info_dataframe.writeStream.foreachBatch(process_batch).start()
     query.awaitTermination()
