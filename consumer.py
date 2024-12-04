109fc2a3dbdb59c3bce3d8d51a837af7e8661729
gmcreynolds <gabrielmc785@gmail.com>
2024-12-04 11:11:17 -0600

    recorded latency for kafka

--- consumer.py
+++ consumer.py
@@ -1,27 +1,37 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
+from pyspark.sql.functions import avg, col, from_json, sum as pyspark_sum
 from pyspark.sql.types import *
 import time
+from typing import List
+
 
 from main import CHECKPOINT_LOCATION
 
 KAFKA_TOPIC_NAME = "posStreaming"
 KAFKA_BOOTSTRAP_SERVERS = "localhost:9092"
 
+latencies: List[float] = []
+
+
 def process_batch(df, epoch_id):
-    aggregated_df = df.groupBy("store_id", "item_id").agg(sum("quantity").alias("delta_quantity")).orderBy(
+    aggregated_df = df.groupBy("store_id", "item_id").agg(pyspark_sum("quantity").alias("delta_quantity")).orderBy(
         "delta_quantity", ascending=True)
     end_time = time.time()
-    duration = end_time - df.select("sent_time").first().sent_time
-    print(f"Batch {epoch_id} GroupBy operation took {duration:.5f} seconds")
+    avg_sent_time = df.agg(avg("sent_time")).first()[0]
+    latency = end_time - avg_sent_time
+    if epoch_id > 0:
+        # skip the first batch
+        latencies.append(latency)
+        print(f"Avg. latency {epoch_id}: {sum(latencies) / len(latencies)}")
     aggregated_df.show()
+
 
 if __name__ == "__main__":
     spark = (
         SparkSession.builder.appName("CIS533 POS Streaming Consumer")
         .master("local[*]")
         .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.3")
-        #.config("spark.streaming.concurrentJobs", "4")
+        # .config("spark.streaming.concurrentJobs", "8")
         .getOrCreate()
     )
     spark.sparkContext.setLogLevel("ERROR")
